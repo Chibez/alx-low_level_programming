abb67d967154596a446ba3ee3c91fef69fe9d243
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-07 15:35:23 -0700

    Size is not grandeur, and territory does not make a nation

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,16 +2,14 @@
 
 /**
  * main - Entry point
- * 
- * Return: (0) Indicates program's exit status
- */
-int main(void)
+ * Return: (0) indicates program's exit status
+*/
+int main (void)
 {
-    printf("Size of a char: %zu byte(s)\n", sizeof(char));
-    printf("Size of a int: %zu byte(s)\n", sizeof(int));
-    printf("Size of a long: %zu byte(s)\n", sizeof(long));
-    printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
-    printf("Size of a float: %zu byte(s)\n", sizeof(float));
-
-    return (0);
-}
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long: %zu byte(s)\n", sizeof(long));
+	printf("Size of a long long: %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+	return (0);
+	}
