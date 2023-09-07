09d082ef36462f007ef7a0423a837e086b1ffd0f
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-07 13:42:41 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+/**
+ * main - Entry point
+ *
+ * Return: Always 0  Indicates program's exit status
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\"");
+	return (0);
+}
