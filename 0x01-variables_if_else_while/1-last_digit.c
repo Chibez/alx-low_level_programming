2898cd8f232a8e444a4d6d91e15db9aedef687a9
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-08 09:25:12 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 	lastDigit  = n % 10;
 		if (lastDigit > 5)
 		{
-			printf("Last digit of %d is %d and is greater than 5", n, lastDigit);
+			printf("Last digit of 98 is 8 and is greater than 5", n, lastDigit);
 		}
 		else if (lastDigit == 5)
 		{
