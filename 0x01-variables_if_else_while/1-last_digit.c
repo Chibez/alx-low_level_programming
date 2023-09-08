ccc5669baadfd79a3f1a947bffa1eee5705cfda4
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-08 14:32:33 -0700

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,15 +22,13 @@ int main(void)
 		}
 		else if (lastDigit < 0)
 		{
-			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
+			printf("Last digit of %d is %d and is less than 6\n", n, lastDigit);
 		}
 		else if (lastDigit > 0 && lastDigit <= 5)
 		{
 			printf("Last digit of %d is %d and less than 6 and not 0\n", n, lastDigit);
 		}
 		else
-		
 			printf("Last digit of %d is %d and is 0\n", n, lastDigit);
-		
 	return (0);
 }
