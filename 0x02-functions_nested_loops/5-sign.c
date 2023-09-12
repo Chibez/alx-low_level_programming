1701685bfd7634f86ad19b5c0f3d444b84fb4261
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-12 15:05:31 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Print the sign of an integer.
+ *
+ * @n: The integer to be evaluated.
+ *
+ * Return: 1 if n is positive, 0 if n is zero, -1 if n is negative.
+ */
+int print_sign(int n)
+{
+	if(n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+	return 0;
+}
