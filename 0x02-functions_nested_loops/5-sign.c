2b2c12369d87326e6df58057bcc67a4b8120cb83
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-12 15:09:11 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,7 +8,7 @@
  */
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 		_putchar('+');
 		return (1);
@@ -24,5 +24,5 @@ int print_sign(int n)
 		return (-1);
 	}
 
-	return 0;
+	return (0);
 }
