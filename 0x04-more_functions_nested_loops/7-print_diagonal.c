55f98bdcbfa43ee27d0e1f1977bb47922ea01165
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-15 14:10:48 -0700

    am diagonally parked

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,15 +10,20 @@
 void print_diagonal(int n)
 {
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
+
 		int i, j;
 
 		for (i = 0; i < n; i++)
 		{
-			(j = 0; j <= i; j++);
+			for (j = 0; j < i; j++)
+			{
 				_putchar(' ');
+			}
 			_putchar('\\');
 			_putchar('\n');
 		}
