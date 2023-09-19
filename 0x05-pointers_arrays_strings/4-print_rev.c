5454f5fabec16282b1c408ea05f389c2d846c6fc
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-18 20:11:40 -0700

    I can only go one way

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,20 @@
 #include "main.h"
+#include <unistd.h>
 
 /**
  * print_rev - Prints a string in reverse followed by a newline.
  *
  * @s: A pointer to the string to be printed in reverse.
  */
+
 void print_rev(char *s)
 {
-	int length = 0;
 	int i;
-	
-	while (s[length] != '\0')
-	{
-		length++;
-	}
+	int monk = 0;
 
-	for (i = length - 1; i >= 0; i--)
-	{
+	for (i = 0; s[i] != '\0'; i++)
+		monk++;
+	for (i = monk - 1; i >= 0; i--)
 		_putchar(s[i]);
-	}
-	
 	_putchar('\n');
 }
-
