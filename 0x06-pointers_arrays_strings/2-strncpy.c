da61f8a2cd23fbfef709f25d4ff8c80730965498
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-09-21 14:32:08 -0700

    strncpy copy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,6 +13,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+
 	for (i = 0; src[i] && i < n; i++)
 	{
 		dest[i] = src[i];
@@ -21,5 +22,5 @@ char *_strncpy(char *dest, char *src, int n)
 	{
 		dest[i] = '\0';
 	}
-	return dest;
+	return (dest);
 }
