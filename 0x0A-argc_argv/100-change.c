6e228b7eb768445c825f64466ba6a40198fe2c07
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-02 18:44:51 -0700

    minimum number of coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,31 +2,52 @@
 #include <stdlib.h>
 
 /**
- * main - Prints the minimum number
- * of coins required to make change.
- * @argc: The number of arguments
- * passed to the program.
+ * main - Prints the minimum number of coins required to make change.
+ * @argc: The number of arguments passed to the program.
  * @argv: An array of pointers to the arguments.
  *
  * Return: 0 on success, 1 on error.
  */
+
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
-	int deno[] = {25, 10, 5, 2, 1};
-	int i;
+	int myCents, myCoins = 0;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	cents = atoi(argv[1]);
-	for (i = 0; i < 5; i++)
+
+	myCents = atoi(argv[1]);
+
+	while (myCents > 0)
 	{
-		coins += cents / deno[i];
-		cents %= deno[i];
+		myCoins++;
+		if ((myCents - 25) >= 0)
+		{
+			myCents -= 25;
+			continue;
+		}
+		if ((myCents - 10) >= 0)
+		{
+			myCents -= 10;
+			continue;
+		}
+		if ((myCents - 5) >= 0)
+		{
+			myCents -= 5;
+			continue;
+		}
+		if ((myCents - 2) >= 0)
+		{
+			myCents -= 2;
+			continue;
+		}
+		myCents--;
 	}
-	printf("%d\n", coins);
+
+	printf("%d\n", myCoins);
+
 	return (0);
 }
