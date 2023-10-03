e417007ec5b04aae46f3ea0172ca147b2bc8c701
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-02 17:57:47 -0700

    new pj on stattic library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -6,4 +6,22 @@
  * Return: sucesss
  */
 
-int _atoi(char *s);
+int _atoi(char *s)
+{
+	int si = 1;
+	unsigned int num = 0;
+
+	do {
+		if (*s == '-')
+			si *= -1;
+
+		else if (*s >= '0' && *s <= '9')
+			num = (num * 10) + (*s - '0');
+
+		else if (num > 0)
+			break;
+
+	} while (*s++);
+
+	return (num * si);
+}
