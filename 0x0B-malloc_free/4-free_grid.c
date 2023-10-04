8a1e16c300d89f07d84ecaa4004d0374950e0fed
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-04 09:27:18 -0700

    dimensional grid previously

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Allocates a 2D array of integers.
+ * @grid: The grid of the 2D array.
+ * @height: The height of the 2D array.
+ *
+ * Return: A pointer to the allocated 2D array, or NULL on failure.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int mak;
+	
+	for (mak = 0; mak < height; mak++)
+	{
+		free(grid[mak]);
+	}
+
+	free(grid);
+}
