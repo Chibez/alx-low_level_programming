c6d2973149a65b71d3b0b2e1cfb0c89eda941c82
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-10 06:44:09 -0700

    book is a man's best friend

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include "dog.h"
-
+/**
+ * print_dog - Prints the contents of a struct dog.
+ * @d: A pointer to the dog structure to print.
+ *
+ * Description: This function prints the information about a dog
+ * including its name, age, and owner.
+ * If any element of the dog structure
+ * is NULL (name or owner), it prints "(nil)" for that element.
+ */
 void print_dog(struct dog *d)
 {
 	if (d != NULL)
@@ -22,6 +30,6 @@ void print_dog(struct dog *d)
 		else
 		{
 			printf("(nil)\n");
-        }
-    }
+		}
+	}
 }
