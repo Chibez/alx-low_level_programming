7442cac4aaa784ebc62226803175276feb9e4566
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-10 05:24:09 -0700

    computes the absolute value

--- 0x0D-preprocessor/3-main.c
+++ 0x0D-preprocessor/3-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "3-function_like_macro.h"
 #include "3-function_like_macro.h"
+
 /**
  * main - check the code
  *
@@ -8,11 +9,11 @@
  */
 int main(void)
 {
-	int i;
-	int j;
+    int i;
+    int j;
 
-	i = ABS(-98) * 10;
-	j = ABS(98) * 10;
-	printf("%d, %d\n", i, j);
-	return (0);
+    i = ABS(-98) * 10;
+    j = ABS(98) * 10;
+    printf("%d, %d\n", i, j);
+    return (0);
 }
