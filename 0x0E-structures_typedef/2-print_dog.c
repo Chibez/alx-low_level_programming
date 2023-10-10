c50da91b3c7bc70e2814dd94cc2929427f7a8ede
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-10 06:37:55 -0700

    book is a man's best friend

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "dog.h"
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: ");
+		if (d->name != NULL)
+		{
+			printf("%s\n", d->name);
+		} else
+		{
+			printf("(nil)\n");
+		}
+		printf("Age: %.6f\n", d->age);
+		printf("Owner: ");
+		if (d->owner != NULL)
+		{
+			printf("%s\n", d->owner);
+		}
+		else
+		{
+			printf("(nil)\n");
+        }
+    }
+}
