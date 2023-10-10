3de0fbeee6b703222898e5924b1bdbd6a76d9a7e
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-10 05:53:49 -0700

    Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,65 +1,18 @@
-#ifndef DOG_H
-#define DOG_H
-
+#ifndef MAIN_H
+#define MAIN_H
 /**
- * struct dog - A new type describing a dog.
+ * struct dog - A structure representing information about a dog.
  * @name: The name of the dog.
  * @age: The age of the dog.
  * @owner: The owner of the dog.
+ *
+ * Description: This structure is used to store information about a dog,
+ * including its name, age, and the name of its owner.
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
-
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
-
-/**
- * init_dog - Initializes a dog structure.
- * @d: A pointer to the dog structure to initialize.
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Description: This function initializes the members of a dog structure
- * with the provided values for name, age, and owner.
- */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-
-/**
- * print_dog - Prints information about a dog.
- * @d: A pointer to the dog structure to print.
- *
- * Description: This function prints the name, age, and owner of the dog.
- */
-void print_dog(struct dog *d);
-
-/**
- * new_dog - Creates a new dog structure.
- * @name: The name of the new dog.
- * @age: The age of the new dog.
- * @owner: The owner of the new dog.
- * Return: A pointer to the newly created dog structure,
- * or NULL on failure.
- * and initializes its members with the provided
- * values for name, age, and owner.
- */
-dog_t *new_dog(char *name, float age, char *owner);
-
-/**
- * free_dog - Frees memory allocated for a dog structure.
- * @d: A pointer to the dog structure to free.
- *
- * Description: This function frees the memory
- * allocated for a dog structure
- * created using the new_dog function.
- */
-void free_dog(dog_t *d);
-
 #endif
-
