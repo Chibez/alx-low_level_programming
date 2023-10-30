9ac7cac606cf60e1825aacf8daba16397887f25e
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-30 04:44:56 -0700

    creates

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -3,7 +3,20 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "main.h"
-
+/**
+ * my_function - This is a brief description of what my_function does.
+ *
+ * @param1: Description of the first parameter
+ * @param2: Description of the second parameter
+ *
+ * A more detailed description of the function goes here,
+ * explaining its purpose
+ * and behavior. You can also mention any relevant
+ * details about the parameters,
+ * return value, or any special considerations.
+ *
+ * Return: Description of the return value (if applicable).
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int fd, res, len = 0;
