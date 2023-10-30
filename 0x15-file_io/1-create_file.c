7909fad7245efd52c83dae927fde0f47dba9f9da
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-10-30 04:38:42 -0700

    creates

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, res, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	while (text_content[len] != '\0')
+		len++;
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	res = write(fd, text_content, len);
+	if (res == -1 || res != len)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	close(fd);
+	return (1);
+}
