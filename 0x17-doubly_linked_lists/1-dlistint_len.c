d044414cca702f678d7b60c4b8fe2771b7e8bf1f
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-12-07 16:05:30 -0800

    length

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,19 +2,19 @@
 
 /**
  * dlistint_len - returns the number of elements in a dlistint_t list.
- * @head: pointer to the list.
+ * @h: pointer to the list.
  * Return: number of nodes.
  **/
 size_t dlistint_len(const dlistint_t *h)
 {
-    const dlistint_t *current_node = h;
-    size_t node_count = 0;
+	const dlistint_t *current_node = h;
+	size_t node_count = 0;
 
-    while (current_node)
-    {
-        node_count++;
-        current_node = current_node->next;
-    }
+	while (current_node)
+	{
+		node_count++;
+		current_node = current_node->next;
+	}
 
-    return (node_count);
+	return (node_count);
 }
