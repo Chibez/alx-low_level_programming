54dbe373f6c12ef151cce8a96efebc0c65f56a93
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2023-12-22 15:48:41 -0800

    djb2 algorithm

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int coun;
+
+	hash = 5381;
+	while ((coun = *str++))
+		hash = ((hash << 5) + hash) + coun; /* hash * 33 + coun */
+
+	return (hash);
+}
